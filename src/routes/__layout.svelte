05ebad7ce6ad46f4cf9d7978a4ebc94bf2522da3
Sibbe Heijne <sibbe.heijne@divotion.com>
2021-11-27 18:29:13 +0100

    Create landing page with logo and brand name

---
+++ src/routes/__layout.svelte
@@ -0,0 +1,24 @@
+<script lang="ts">
+	import '../app.css';
+
+	import Footer from '$lib/footer';
+</script>
+
+<style>
+	#app {
+			background-image: url(/background.png);
+			background-position: center;
+      background-size: cover;
+			display: grid;
+			height: 100vh;
+			grid-template-rows: 1fr 5rem;
+	}
+</style>
+
+<div id="app">
+	<main>
+		<slot />
+	</main>
+
+	<Footer/>
+</div>
---
+++ src/routes/index.svelte
@@ -0,0 +1,25 @@
+<script context="module" lang="ts">
+	export const prerender = true;
+</script>
+
+<script lang="ts">
+	import Logo from '$lib/logo';
+</script>
+
+<style>
+	section {
+			align-items: center;
+      display: flex;
+			justify-content: center;
+			height: 100%;
+			padding: 2rem;
+	}
+</style>
+
+<svelte:head>
+	<title>Ovenwand</title>
+</svelte:head>
+
+<section>
+	<Logo/>
+</section>
