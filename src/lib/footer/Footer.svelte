18c6ba62d721e2349895552e065db2da6d4cc07d
Sibbe Heijne <sibbe.heijne@divotion.com>
2021-11-27 18:29:13 +0100

    Create landing page with logo and brand name

---
+++ src/lib/footer/Footer.svelte
@@ -0,0 +1,18 @@
+<script lang="ts">
+	const name = 'Ovenwand';
+	const currentYear = (new Date()).getFullYear();
+</script>
+
+<style>
+	footer {
+			align-items: center;
+			color: white;
+      display: flex;
+			font-size: 1.5rem;
+			justify-content: center;
+	}
+</style>
+
+<footer>
+	<span>&copy; {name} {currentYear}</span>
+</footer>
