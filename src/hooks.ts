ee9f7b7dfe748acb3f076f2f00dcb5eab6f79550
Sibbe Heijne <sibbe@ovenwand.com>
2022-01-27 10:50:36 +0100

    Replace reference to headers in hooks.ts in accordance to latest svelte-kit

--- src/hooks.ts
+++ src/hooks.ts
@@ -1,6 +1,6 @@
 import type { GetSession } from '@sveltejs/kit';
 
-export const getSession: GetSession = async ({ headers }) => {
-    const { referer } = headers;
-    return { referer };
-}
+export const getSession: GetSession = async ({ request }) => {
+	const referer = request.headers.get('referer');
+	return { referer };
+};
