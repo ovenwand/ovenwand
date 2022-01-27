c130ac6d62566451848f82093690d2f069cbe1ca
Sibbe Heijne <sibbe@ovenwand.com>
2022-01-27 10:49:09 +0100

    Add default and clean layouts

---
+++ src/modules/app/components/layouts/DefaultLayout.svelte
@@ -0,0 +1,22 @@
+<script lang="ts">
+	import '../../../../app.css';
+	import Footer from '$modules/app/components/footer';
+</script>
+
+<div id="app" class="h-min-screen">
+	<main>
+		<slot />
+	</main>
+
+	<Footer />
+</div>
+
+<style>
+	#app {
+		background-image: url(/background.png);
+		background-position: center;
+		background-size: cover;
+		display: grid;
+		grid-template-rows: 1fr 5rem;
+	}
+</style>
--- src/routes/__layout.svelte
+++ src/routes/__layout.svelte
@@ -1,24 +1,7 @@
 <script lang="ts">
-	import '../app.css';
-
-	import Footer from '$modules/app/components/footer';
+	import { DefaultLayout } from '$modules/app/components/layouts';
 </script>
 
-<style>
-	#app {
-			background-image: url(/background.png);
-			background-position: center;
-      background-size: cover;
-			display: grid;
-			grid-template-rows: 1fr 5rem;
-      min-height: 100vh;
-	}
-</style>
-
-<div id="app">
-	<main>
-		<slot />
-	</main>
-
-	<Footer/>
-</div>
+<DefaultLayout>
+	<slot />
+</DefaultLayout>
