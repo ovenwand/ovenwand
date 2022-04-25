7c91030083e0175030bf9449018e5f504a36e733
Sibbe Heijne <sibbe.heijne@kpn.com>
2022-04-25 16:38:19 +0200

    chore(apps.ovenwand.com): implement named layout for home page

---
+++ packages/apps/ovenwand.com/src/routes/index@with-footer.svelte
@@ -0,0 +1,28 @@
+<script context="module" lang="ts">
+	export const prerender = true;
+</script>
+
+<script lang="ts">
+	import { Column, Grid } from '@ovenwand/ui.grid';
+	import { Logo, Navigation } from '$lib/components';
+</script>
+
+<svelte:head>
+	<title>Ovenwand</title>
+</svelte:head>
+
+<Grid class="h-full">
+	<Column
+		columns={4}
+		offset={4}
+		class="
+				flex
+				flex-col
+				items-center
+				justify-center
+		 "
+	>
+		<Logo />
+		<Navigation class="py-12" />
+	</Column>
+</Grid>
