eab8f0e6d7de0a6dc4f80b14253b87b7dc6eb335
Sibbe Heijne <sibbe@ovenwand.com>
2022-04-25 16:37:54 +0200

    chore(apps.ovenwand.com): replace custom footer code with reusable components - implements ui.grid - replaces custom styles with tailwind utilities

--- packages/apps/ovenwand.com/src/lib/components/footer/Footer.svelte
+++ packages/apps/ovenwand.com/src/lib/components/footer/Footer.svelte
@@ -1,22 +1,18 @@
 <script lang="ts">
 	import { fly } from 'svelte/transition';
+	import { Column, Grid } from '@ovenwand/ui.grid';
 
 	const name = 'Ovenwand';
 	const currentYear = new Date().getFullYear();
 	$: copyright = `&copy; ${name} ${currentYear}`;
 </script>
 
-<footer in:fly={{ delay: 250, duration: 300, y: 8 }} out:fly={{ duration: 200 }}>
-	<slot {copyright}>
-		<span class="display-4 weight-normal">{@html copyright}</span>
-	</slot>
+<footer class="text-white" in:fly={{ delay: 250, duration: 300, y: 8 }} out:fly={{ duration: 200 }}>
+	<Grid>
+		<Column class="flex justify-center items-center">
+			<slot {copyright}>
+				<span class="display-4 weight-normal">{@html copyright}</span>
+			</slot>
+		</Column>
+	</Grid>
 </footer>
-
-<style lang="postcss">
-	footer {
-		align-items: center;
-		color: white;
-		display: flex;
-		justify-content: center;
-	}
-</style>
