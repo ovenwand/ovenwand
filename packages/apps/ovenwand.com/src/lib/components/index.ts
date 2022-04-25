59305b2d96a8a4a8e7250060b77fff4a4760597a
Sibbe Heijne <sibbe.heijne@kpn.com>
2022-04-25 16:06:32 +0200

    chore(apps.ovenwand.com): refactor navigation into separate components

--- packages/apps/ovenwand.com/src/lib/components/index.ts
+++ packages/apps/ovenwand.com/src/lib/components/index.ts
@@ -1,3 +1,4 @@
+export * from './navigation';
 export * from './footer';
 export * from './loader';
 export * from './logo';
---
+++ packages/apps/ovenwand.com/src/lib/components/navigation/SideNavigation.svelte
@@ -0,0 +1,41 @@
+<script lang="ts">
+	import { createClassName } from '@ovenwand/util.browser';
+
+	let className: string;
+	export { className as class };
+
+	const items = [
+		{
+			label: 'Experiments',
+			href: '/experiments/pendulum'
+		},
+		{
+			label: 'Tasks',
+			href: '/productivity/tasks'
+		}
+	];
+
+	$: navClassName = createClassName({
+		flex: true,
+		[className]: className
+	});
+</script>
+
+<nav class={navClassName}>
+	<ul>
+		{#each items as item}
+			<li class="flex items-center">
+				{#if item.href}
+					<a href={item.href} on:click>
+						{item.label}
+					</a>
+				{:else}
+					<button on:click={() => item.action()} on:click>
+						{item.label}
+					</button>
+				{/if}
+			</li>
+		{/each}
+	</ul>
+	<slot />
+</nav>
