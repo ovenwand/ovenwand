8d8d7e4a477a607529d6763c0cb58ba11fcdc2c0
Sibbe Heijne <sibbe@ovenwand.com>
2022-04-25 16:15:40 +0200

    feat(apps.ovenwand.com): add header component

---
+++ packages/apps/ovenwand.com/src/lib/components/header/Header.svelte
@@ -0,0 +1,17 @@
+<script lang="ts">
+	import { Grid, Column } from '@ovenwand/ui.grid';
+	import { Logo } from '../logo';
+	import { Navigation } from '../navigation';
+</script>
+
+<Grid>
+	<Column class="flex">
+		<a href="/">
+			<Logo size="small" />
+		</a>
+
+		<div class="flex-grow" />
+
+		<Navigation />
+	</Column>
+</Grid>
