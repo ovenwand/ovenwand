7ace3c1515b6abb5c1473417c6808688e1884a55
Sibbe Heijne <sibbe.heijne@kpn.com>
2022-04-25 15:52:38 +0200

    fix(ui.layout): header and footer alignment in default layout

--- packages/ui/layout/src/DefaultLayout.svelte
+++ packages/ui/layout/src/DefaultLayout.svelte
@@ -1,16 +1,45 @@
-<div id="app" class="min-h-screen bg-center bg-cover grid">
-	<slot name="before-content" />
+<script lang="ts">
+	import { createClassName } from '@ovenwand/util.browser';
+	export let header = false;
+	export let footer = false;
+
+	$: appClassName = createClassName({
+		'min-h-screen bg-center bg-cover grid': true,
+		'has-header': header,
+		'has-footer': footer
+	});
+</script>
+
+<div id="app" class={appClassName}>
+	{#if header}
+		<slot name="before-content" />
+	{/if}
 
 	<main>
 		<slot />
 	</main>
 
-	<slot name="after-content" />
+	{#if footer}
+		<slot name="after-content" />
+	{/if}
 </div>
+
+<slot name="outside" />
 
 <style lang="postcss">
 	#app {
 		background-image: url(/background.png);
-		grid-template-rows: 1fr 5rem;
+	}
+
+	#app.has-header {
+		grid-template-rows: min-content 1fr;
+	}
+
+	#app.has-footer {
+		grid-template-rows: 1fr min-content;
+	}
+
+	#app.has-header.has-footer {
+		grid-template-rows: min-content 1fr min-content;
 	}
 </style>
