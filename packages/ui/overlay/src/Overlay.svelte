bb0d9003973d22c62650948f9a3be73ca9e95d8e
Sibbe Heijne <sibbe@ovenwand.com>
2022-04-25 16:22:45 +0200

    feat(ui.overlay): add overlay package

---
+++ packages/ui/overlay/src/Overlay.svelte
@@ -0,0 +1,15 @@
+<script lang="ts">
+	import { fade } from 'svelte/transition';
+	import { createClassName } from '@ovenwand/util.browser';
+
+	let className: string = null;
+	export { className as class };
+
+	$: overlayClassName = createClassName({
+		[className]: className
+	});
+</script>
+
+<div class="fixed bg-gray-900/80 inset-0 {overlayClassName}" transition:fade on:click|self>
+	<slot />
+</div>
