ebc5c842dc94130a49fe466cba470c1e2d9d314f
Sibbe Heijne <sibbe.heijne@kpn.com>
2022-04-26 15:36:17 +0200

    feat(ui.modal): make modal window responsive

--- packages/ui/modal/src/Modal.svelte
+++ packages/ui/modal/src/Modal.svelte
@@ -2,13 +2,17 @@
 	import { Column, Grid } from '@ovenwand/ui.grid';
 	import { Overlay } from '@ovenwand/ui.overlay';
 	import { Sheet } from '@ovenwand/ui.sheet';
+	import { useMedia } from '@ovenwand/util.svelte';
+
 	export let active = false;
+
+	const { portrait } = useMedia();
 </script>
 
 {#if active}
 	<Overlay class="flex items-center justify-center" on:click={() => (active = false)}>
 		<Grid gap-outside={false}>
-			<Column columns={8} offset={2}>
+			<Column columns={$portrait ? 10 : 8} offset={$portrait ? 1 : 2}>
 				<Sheet padding background rounded shadow>
 					<slot />
 				</Sheet>
