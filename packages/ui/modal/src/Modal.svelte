6d293aed62a9480cff7897d7c3f0c2f7c26f7156
Sibbe Heijne <sibbe@ovenwand.com>
2022-04-26 15:37:18 +0200

    fix(ui.modal): make modal overlay clickable

--- packages/ui/modal/src/Modal.svelte
+++ packages/ui/modal/src/Modal.svelte
@@ -11,8 +11,8 @@
 
 {#if active}
 	<Overlay class="flex items-center justify-center" on:click={() => (active = false)}>
-		<Grid gap-outside={false}>
-			<Column columns={$portrait ? 10 : 8} offset={$portrait ? 1 : 2}>
+		<Grid class="pointer-events-none" gap-outside={false}>
+			<Column class="pointer-events-auto" columns={$portrait ? 10 : 8} offset={$portrait ? 1 : 2}>
 				<Sheet padding background rounded shadow>
 					<slot />
 				</Sheet>
