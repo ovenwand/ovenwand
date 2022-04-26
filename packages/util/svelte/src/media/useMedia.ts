05920b3361d0ee8bac3c633616a0f676cf184411
Sibbe Heijne <sibbe.heijne@kpn.com>
2022-04-26 19:58:51 +0200

    feat(util.svelte): refactor useMedia to allow greater/lower than usage

--- packages/util/svelte/src/media/useMedia.ts
+++ packages/util/svelte/src/media/useMedia.ts
@@ -3,39 +3,35 @@ import type { Readable, Writable } from 'svelte/store';
 import { derived, writable } from 'svelte/store';
 import { isClient } from '@ovenwand/util.browser';
 
-export type Breakpoint = 'xs' | 'sm' | 'md' | 'lg' | 'xl' | 'xxl';
+export type Breakpoint = number;
+export type BreakpointName = 'xs' | 'sm' | 'md' | 'lg' | 'xl' | 'xxl';
 export type Orientation = 'portrait' | 'landscape';
 
-const breakpoints = {
-	sm: 640,
-	md: 768,
-	lg: 1024,
-	xl: 1280,
-	xxl: 1536
+const breakpoints: UseMedia['breakpoints'] = {
+	xs: 0,
+	sm: 1,
+	md: 2,
+	lg: 3,
+	xl: 4,
+	xxl: 5
 };
 
 const breakpoint: UseMedia['breakpoint'] = writable(findBreakpoint());
-const orientation: UseMedia['orientation'] = writable(findOrientation());
 const xs: UseMedia['xs'] = derived(breakpoint, matchBreakpoint('xs'));
 const sm: UseMedia['sm'] = derived(breakpoint, matchBreakpoint('sm'));
 const md: UseMedia['md'] = derived(breakpoint, matchBreakpoint('md'));
 const lg: UseMedia['lg'] = derived(breakpoint, matchBreakpoint('lg'));
 const xl: UseMedia['xl'] = derived(breakpoint, matchBreakpoint('xl'));
 const xxl: UseMedia['xxl'] = derived(breakpoint, matchBreakpoint('xxl'));
+
+const orientation: UseMedia['orientation'] = writable(findOrientation());
 const portrait: UseMedia['portrait'] = derived(orientation, matchOrientation('portrait'));
 const landscape: UseMedia['landscape'] = derived(orientation, matchOrientation('landscape'));
 
-function matchBreakpoint(name: Breakpoint) {
-	const BREAKPOINTS: Record<Breakpoint, number> = {
-		xs: 0,
-		sm: 1,
-		md: 2,
-		lg: 3,
-		xl: 4,
-		xxl: 5
-	};
+let created = false;
 
-	return (breakpoint: Breakpoint) => BREAKPOINTS[breakpoint] >= BREAKPOINTS[name];
+function matchBreakpoint(name: BreakpointName) {
+	return (breakpoint: Breakpoint) => breakpoint >= breakpoints[name];
 }
 
 function matchOrientation(name: Orientation) {
@@ -44,25 +40,25 @@ function matchOrientation(name: Orientation) {
 
 function findBreakpoint(): Breakpoint {
 	if (!isClient) {
-		return 'xs';
+		return breakpoints.xs;
 	}
 
 	const { clientWidth } = document.documentElement;
 
 	let $breakpoint: Breakpoint;
 
-	if (clientWidth < breakpoints.sm) {
-		$breakpoint = 'xs';
-	} else if (clientWidth < breakpoints.md) {
-		$breakpoint = 'sm';
-	} else if (clientWidth < breakpoints.lg) {
-		$breakpoint = 'md';
-	} else if (clientWidth < breakpoints.xl) {
-		$breakpoint = 'lg';
-	} else if (clientWidth < breakpoints.xxl) {
-		$breakpoint = 'xl';
+	if (clientWidth < 640) {
+		$breakpoint = breakpoints.xs;
+	} else if (clientWidth < 768) {
+		$breakpoint = breakpoints.sm;
+	} else if (clientWidth < 1024) {
+		$breakpoint = breakpoints.md;
+	} else if (clientWidth < 1280) {
+		$breakpoint = breakpoints.lg;
+	} else if (clientWidth < 1536) {
+		$breakpoint = breakpoints.xl;
 	} else {
-		$breakpoint = 'xxl';
+		$breakpoint = breakpoints.xxl;
 	}
 
 	return $breakpoint;
@@ -97,32 +93,33 @@ export interface UseMedia {
 	portrait: Readable<boolean>;
 	landscape: Readable<boolean>;
 	orientation: Writable<Orientation>;
+	breakpoints: Record<BreakpointName, Breakpoint>;
 }
 
 export function useMedia(): UseMedia {
-	if (isClient) {
+	if (isClient && !created) {
 		window.addEventListener('resize', setBreakpoint);
 		window.addEventListener('resize', setOrientation);
 
-		onDestroy(() => {
-			window.removeEventListener('resize', setBreakpoint);
-			window.removeEventListener('resize', setOrientation);
-		});
-
 		setBreakpoint();
 		setOrientation();
+
+		created = true;
 	}
 
 	return {
+		breakpoints,
+
+		breakpoint,
 		xs,
 		sm,
 		md,
 		lg,
 		xl,
 		xxl,
-		breakpoint,
+
+		orientation,
 		portrait,
-		landscape,
-		orientation
+		landscape
 	};
 }
