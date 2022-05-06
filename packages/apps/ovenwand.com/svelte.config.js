c191195cf5e122fe934c95e7d5dfee54dd29daff
Sibbe Heijne <sibbe@ovenwand.com>
2022-05-06 22:15:19 +0200

    feat(apps.ovenwand.com): add database schema and utilities

--- packages/apps/ovenwand.com/svelte.config.js
+++ packages/apps/ovenwand.com/svelte.config.js
@@ -3,7 +3,7 @@ import config from '@ovenwand/config/svelte';
 
 config.kit.vite.resolve.alias = {
 	$lib: path.resolve('src/lib'),
-	$modules: path.resolve('src/modules')
+	$database: path.resolve('src/database'),
 };
 
 export default config;
