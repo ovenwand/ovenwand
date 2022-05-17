61376f711b5525eaea441704e6c57761a7e23cd3
Sibbe Heijne <sibbe.heijne@kpn.com>
2022-05-17 15:55:20 +0200

    chore(apps.ovenwand.com): add types to database utils

--- packages/apps/ovenwand.com/src/database/query.ts
+++ packages/apps/ovenwand.com/src/database/query.ts
@@ -1,7 +1,7 @@
-export async function gql(
-	query: string,
-	variables: Record<string, unknown> = {}
-): Promise<{ data: {}; errors: {}[] }> {
+export async function gql<
+	Data extends Record<string, unknown> = {},
+	Errors extends { message: string }[] = []
+>(query: string, variables?: Record<string, unknown>): Promise<{ data: Data; errors: Errors }> {
 	const response = await fetch(import.meta.env.VITE_FAUNA_URL, {
 		method: 'POST',
 		headers: {
