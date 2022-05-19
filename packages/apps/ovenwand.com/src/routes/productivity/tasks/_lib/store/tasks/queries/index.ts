0c45c42720fd1bbce42732049c9c7587eb2a2917
Sibbe Heijne <sibbe@ovenwand.com>
2022-05-19 12:53:19 +0200

    chore(ovenwand.com): remove productivity routes

--- packages/apps/ovenwand.com/src/routes/productivity/tasks/_lib/store/tasks/queries/index.ts
+++
@@ -1,4 +0,0 @@
-export { default as FindAllTasks } from './FindAllTasks.gql?raw';
-export { default as CreateTask } from './CreateTask.gql?raw';
-export { default as DeleteTask } from './DeleteTask.gql?raw';
-export { default as UpdateTask } from './UpdateTask.gql?raw';
