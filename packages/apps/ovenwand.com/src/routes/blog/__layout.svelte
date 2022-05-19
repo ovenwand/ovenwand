522f2bd0b997ba025e2fcb2d5ffd34f926309b2e
Sibbe Heijne <sibbe.heijne@kpn.com>
2022-05-19 11:21:00 +0200

    feat(ovenwand.com): allow disabling storyblok bridge

--- packages/apps/ovenwand.com/src/routes/blog/__layout.svelte
+++ packages/apps/ovenwand.com/src/routes/blog/__layout.svelte
@@ -1,12 +1,20 @@
-<script context="module">
+<script lang="ts" context="module">
 	import { useContent, Blog, Author, Page, Text } from '$lib/content';
+	import type { LoadInput, LoadOutput } from '@sveltejs/kit';
 
-	useContent({
-		blog: Blog,
-		page: Page,
-		person: Author,
-		text: Text
-	});
+	export async function load({ url }: LoadInput): Promise<LoadOutput> {
+		useContent(
+			{
+				blog: Blog,
+				page: Page,
+				person: Author,
+				text: Text
+			},
+			url.searchParams.has('_storyblok')
+		);
+
+		return {};
+	}
 </script>
 
 <slot />
