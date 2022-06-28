6ba530e55217093e7e1b492c881745d757028978
Sibbe Heijne <sibbe@ovenwand.com>
2022-06-28 14:47:53 +0200

    feat(ui.button): add basic appearance

--- packages/ui/button/src/Button.svelte
+++ packages/ui/button/src/Button.svelte
@@ -1,14 +1,23 @@
 <script lang="ts">
+	import { createClassName } from '@ovenwand/util.browser';
 	export let href: string | null = null;
 	export let type = 'button';
+
+	const buttonClassName = createClassName({
+		block: true,
+		'px-2': true,
+		'py-2': true,
+		'leading-none': true,
+		'text-center': true
+	});
 </script>
 
 {#if href}
-	<a {href} on:click {...$$restProps}>
+	<a class={buttonClassName} {href} sveltekit:prefetch on:click {...$$restProps}>
 		<slot />
 	</a>
 {:else}
-	<button {type} on:click {...$$restProps}>
+	<button class={buttonClassName} {type} on:click {...$$restProps}>
 		<slot />
 	</button>
 {/if}
