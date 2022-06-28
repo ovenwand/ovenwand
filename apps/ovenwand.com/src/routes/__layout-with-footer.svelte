f899d62953dc155526b78c2ccc96f73fea7978ad
Sibbe Heijne <sibbe@ovenwand.com>
2022-06-28 16:21:24 +0200

    feat(ovenwand.com): add page view tracking

--- apps/ovenwand.com/src/routes/__layout-with-footer.svelte
+++ apps/ovenwand.com/src/routes/__layout-with-footer.svelte
@@ -1,7 +1,13 @@
 <script lang="ts">
 	import '../app.css';
+	import { useAnalytics } from '@ovenwand/analytics';
 	import { DefaultLayout } from '@ovenwand/ui';
+	import { browser } from '$app/env';
+	import { page } from '$app/stores';
 	import { ApplicationSidebar, Footer } from '$lib/components';
+
+	const { trackPageView } = useAnalytics(import.meta.env.VITE_ANALYTICS_PROJECT);
+	$: $page.url.pathname, browser && trackPageView();
 </script>
 
 <DefaultLayout footer>
