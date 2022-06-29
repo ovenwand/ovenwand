c2d606ad0f88a0cab7fa0924f96df6305ee29961
Sibbe Heijne <sibbe@ovenwand.com>
2022-06-29 20:32:45 +0200

    feat(analytics.ovenwand.com): add root latour and app base

---
+++ apps/analytics.ovenwand.com/src/routes/__layout@root.svelte
@@ -0,0 +1,9 @@
+<script lang="ts">
+	import '../app.css';
+	import { DefaultLayout, Notifications } from '@ovenwand/ui';
+</script>
+
+<DefaultLayout class="h-screen" footer>
+	<Notifications top />
+	<slot />
+</DefaultLayout>
