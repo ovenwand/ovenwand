7d9979d686ea10d44f6d9680ffdd2c1c67138383
Sibbe Heijne <sibbe@ovenwand.com>
2022-07-06 22:21:00 +0200

    chore(monitor.ovenwand.com): move dashboard to root and remove redirect

---
+++ apps/monitor.ovenwand.com/src/lib/widgets/index.ts
@@ -0,0 +1,5 @@
+export { default as EventList } from './EventList.svelte';
+export { default as ProjectList } from './ProjectList.svelte';
+export { default as PageViewCounter } from './PageViewCounter.svelte';
+export { default as PageViewMonthChart } from './PageViewMonthChart.svelte';
+export { default as PageViewDaysOfTheWeekChart } from './PageViewDaysOfTheWeekChart.svelte';
--- apps/monitor.ovenwand.com/src/routes/index.svelte
+++ apps/monitor.ovenwand.com/src/routes/index.svelte
@@ -1,8 +1,47 @@
-<script lang="ts" context="module">
-	export async function load() {
-		return {
-			status: 302,
-			redirect: '/dashboard'
-		};
-	}
+<script lang="ts">
+	import { page } from '$app/stores';
+	import { Column, Grid } from '@ovenwand/ui';
+	import { useEvents, /* useProjects,*/ type IEvent, type IProject } from '$lib/store';
+
+	import {
+		PageViewCounter,
+		EventList,
+		/* ProjectList, */ PageViewMonthChart,
+		PageViewDaysOfTheWeekChart
+	} from '$lib/widgets';
+
+	export let data: { projects: IProject[]; events: IEvent[] } = { projects: [], events: [] };
+
+	// const { projects } = useProjects(data.projects);
+	const { events, eventsByProject, eventsToday, eventsInPastWeek, eventsInPastMonth, setProject } =
+		useEvents(data.events);
+
+	$: setProject($page.url.searchParams.get('project'));
 </script>
+
+<Grid>
+	<!--	<Column>-->
+	<!--		<ProjectList projects={$projects} />-->
+	<!--	</Column>-->
+
+	<Column columns={{ md: 4 }}>
+		<PageViewCounter
+			events={$events}
+			eventsToday={$eventsToday}
+			eventsInPastWeek={$eventsInPastWeek}
+			eventsInPastMonth={$eventsInPastMonth}
+		/>
+	</Column>
+
+	<Column columns={{ md: 4 }}>
+		<PageViewMonthChart events={$eventsInPastMonth} />
+	</Column>
+
+	<Column columns={{ md: 4 }}>
+		<PageViewDaysOfTheWeekChart events={$eventsByProject} />
+	</Column>
+
+	<Column columns={{ md: 10 }} offset={{ md: 1 }}>
+		<EventList events={$eventsInPastMonth} />
+	</Column>
+</Grid>
