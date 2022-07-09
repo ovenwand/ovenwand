44ae96833c5163158b960283752766bbad43ccda
Sibbe Heijne <sibbe@ovenwand.com>
2022-07-09 20:14:26 +0200

    fix: add APP_VERSION env variable to monitor and tasks apps

--- apps/tasks.ovenwand.com/vite.config.ts
+++ apps/tasks.ovenwand.com/vite.config.ts
@@ -3,6 +3,11 @@ import { sveltekit } from '@sveltejs/kit/vite';
 
 export default defineConfig({
 	plugins: [sveltekit()],
+
+	define: {
+		'import.meta.env.APP_VERSION': JSON.stringify(process.env.npm_package_version)
+	},
+
 	server: {
 		https: true
 	}
