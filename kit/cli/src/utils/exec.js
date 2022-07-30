2515f6c742bd5e4f09f9388a0f2c73e8c122be15
Sibbe Heijne <sibbe@ovenwand.com>
2022-07-30 21:40:05 +0200

    refactor(cli): add error handling to exec util

--- kit/cli/src/utils/exec.js
+++ kit/cli/src/utils/exec.js
@@ -3,22 +3,25 @@ import { spawn } from 'child_process';
 export async function exec(command, args, options = {}) {
 	return new Promise((resolve, reject) => {
 		let output = '';
+		let error = '';
 
-		const result = (code) => ({
+		const result = (code, output, error) => ({
 			ok: code === 0,
 			code,
-			output
+			output,
+			error
 		});
 
 		const next = (resolve, reject) => {
 			child.on('error', reject);
-			child.on('close', (code) => resolve(result(code)));
+			child.on('close', (code) => resolve(result(code, output, error)));
 		};
 
 		const child = spawn(command, args, { stdio: 'inherit', ...options });
 
 		if (options.stdio === 'pipe') {
 			child.stdout.on('data', (message) => (output += message.toString()));
+			child.stderr.on('data', (message) => (output += message.toString()));
 		}
 
 		if (options.immediate) {
