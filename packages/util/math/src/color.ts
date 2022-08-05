945b990de23058fe1901f2aabde8f685a5a936e8
Sibbe Heijne <sibbe@ovenwand.com>
2022-08-05 22:10:26 +0200

    fix(util.math): debug and simplify color utility
    - Fixes alpha always being 0

--- packages/util/math/src/color.ts
+++ packages/util/math/src/color.ts
@@ -1,39 +1,29 @@
-import { isNumber } from '@ovenwand/util.fp';
+import { isNumber, isString } from '@ovenwand/util.fp';
 
 export function color(r: number, g?: number, b?: number, a?: number): string;
-export function color(...args: [number, number?, number?, number?]): string {
-	args = args.filter(isNumber) as typeof args;
-
-	const [r, g = 0, b = 0, a = 1] = args;
-	const numArgs = args.length;
-	const results: number[] = [0, 0, 0, 0];
-
-	if (numArgs >= 3) {
-		// Argument is a list of component values.
-		results[0] = r;
-		results[1] = g as number;
-		results[2] = b as number;
-	} else if ((numArgs === 1 || numArgs === 2) && typeof r === 'number') {
-		// 'Grayscale' mode.
-
-		/**
-		 * For HSB and HSL, interpret the gray level as a brightness/lightness
-		 * value (they are equivalent when chroma is zero). For RGB, normalize the
-		 * gray level according to the blue maximum.
-		 */
-		results[0] = r;
-		results[1] = r;
-		results[2] = r;
-
-		// Alpha may be undefined, so default it to 100%.
-		if (typeof g === 'number') {
-			results[3] = g;
-		}
-	} else if (numArgs !== 1 && typeof r !== 'string') {
-		throw new Error(`${args}is not a valid color representation.`);
+export function color(r: number, a?: number): string;
+export function color(r: string): string;
+export function color(...args: [number | string, number?, number?, number?]): string {
+	if (isString(args[0])) {
+		return args[0];
 	}
 
-	results[3] = results[3] ?? a;
+	args = args.filter(isNumber) as typeof args;
 
-	return `rgba(${results[0]},${results[1]},${results[2]},${results[3]})`;
+	const numArgs = args.length;
+	let [r, g, b, a = 1] = args as number[];
+
+	if (numArgs === 1 || numArgs === 2) {
+		// 'Grayscale' mode.
+
+		// Alpha may be passed as the second argument.
+		if (isNumber(g)) {
+			a = g;
+		}
+
+		g = r;
+		b = r;
+	}
+
+	return `rgba(${r},${g},${b},${a})`;
 }
