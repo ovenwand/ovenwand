7bcd5d302133a05ee0a74f1b0113d8aa73f8907e
Sibbe Heijne <sibbe@ovenwand.com>
2022-08-09 14:34:24 +0200

    feat(util.fp): add new equality  utils
    - isUndefined
    - isNull
    - isNullish

--- packages/util/fp/src/is.ts
+++ packages/util/fp/src/is.ts
@@ -9,3 +9,15 @@ export function isBoolean(value: unknown): value is boolean {
 export function isString(value: unknown): value is string {
 	return typeof value === 'string';
 }
+
+export function isUndefined(value: unknown): value is undefined {
+	return typeof value === 'undefined';
+}
+
+export function isNull(value: unknown): value is null {
+	return value === null;
+}
+
+export function isNullish(value: unknown): value is null | undefined {
+	return value == null;
+}
