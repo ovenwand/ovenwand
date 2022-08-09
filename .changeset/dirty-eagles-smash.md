b78e216778d27129f2d76635448f73b3cfb66999
Sibbe Heijne <sibbe@ovenwand.com>
2022-08-09 14:41:55 +0200

    refactor(util.browser): replace nullish check with util

---
+++ .changeset/dirty-eagles-smash.md
@@ -0,0 +1,5 @@
+---
+'@ovenwand/util.browser': patch
+---
+
+Replace nullish check with util
--- packages/util/browser/src/element/createStyle.ts
+++ packages/util/browser/src/element/createStyle.ts
@@ -1,10 +1,12 @@
+import { isNullish } from '@ovenwand/util.fp';
+
 export function createStyle(styleMap: Record<string, unknown>): string {
 	let style = '';
 
 	for (const key of Object.keys(styleMap)) {
 		const value = styleMap[key];
 
-		if (value != null) {
+		if (!isNullish(value)) {
 			style += `${key.trim()}: ${String(value).trim()};`;
 		}
 	}
