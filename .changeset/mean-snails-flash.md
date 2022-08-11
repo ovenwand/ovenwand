303a915a7f650c2de8a07c035b717cbf69a6f9f6
Sibbe Heijne <sibbe@ovenwand.com>
2022-08-11 14:15:41 +0200

    fix(cli): accept rest arguments when no app is passed

---
+++ .changeset/mean-snails-flash.md
@@ -0,0 +1,5 @@
+---
+'@ovenwand/kit.cli': patch
+---
+
+Fix dev, build, preview command not passing arguments when no app is passed
--- packages/kit/cli/src/commands/dev.js
+++ packages/kit/cli/src/commands/dev.js
@@ -4,6 +4,9 @@ import { turbo } from '../utils/turbo.js';
 export const dev = createCommand(async (app, options, { command, paths }) => {
 	const arg = options.env ? 'dev:env' : 'dev';
 
+	// Make sure we don't accidentally use an argument as app
+	app = app && !app.startsWith('--') ? app : undefined;
+
 	// Remove the app arg from the args list
 	if (app) {
 		command.args.shift();
