54e1e0d5d28051c355903973642faccef1a1983b
Sibbe Heijne <sibbe@ovenwand.com>
2022-08-11 14:15:41 +0200

    fix(cli): accept rest arguments when no app is passed

--- packages/kit/cli/src/commands/preview.js
+++ packages/kit/cli/src/commands/preview.js
@@ -4,6 +4,9 @@ import { turbo } from '../utils/turbo.js';
 export const preview = createCommand(async (app, options, { command, paths }) => {
 	const arg = options.env ? 'preview:env' : 'preview';
 
+	// Make sure we don't accidentally use an argument as app
+	app = app && !app.startsWith('--') ? app : undefined;
+
 	// Remove the app arg from the args list
 	if (app) {
 		command.args.shift();
