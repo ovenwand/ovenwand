61aa08805fa9cd5e850e501d29ec8c8e900c183b
Sibbe Heijne <sibbe@ovenwand.com>
2022-08-25 18:17:46 +0200

    feat(tasks.ovenwand.com): add Panel component

--- apps/tasks.ovenwand.com/src/lib/components/index.ts
+++ apps/tasks.ovenwand.com/src/lib/components/index.ts
@@ -1,4 +1,5 @@
 export { default as ApplicationState } from './ApplicationState.svelte';
+export { default as Panel } from './Panel.svelte';
 export { default as Pool } from './Pool.svelte';
 export { default as Task } from './Task.svelte';
 export { default as TaskModal } from './TaskModal.svelte';
