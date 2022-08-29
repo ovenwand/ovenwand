b5e9d79ea5daac0795978b1db2c8497b8ec0c991
Sibbe Heijne <sibbe@ovenwand.com>
2022-08-29 19:51:45 +0200

    chore(tasks.ovenwand.com): remove @vitejs/plugin-basic-ssl

--- apps/tasks.ovenwand.com/vite.config.ts
+++ apps/tasks.ovenwand.com/vite.config.ts
@@ -1,16 +1,14 @@
 import { defineConfig } from 'vite';
 import { sveltekit } from '@sveltejs/kit/vite';
-import basicSsl from '@vitejs/plugin-basic-ssl';
 
 export default defineConfig({
-	plugins: [basicSsl(), sveltekit()],
+	plugins: [sveltekit()],
 
 	define: {
 		'import.meta.env.APP_VERSION': JSON.stringify(process.env.npm_package_version)
 	},
 
 	server: {
-		https: true,
 		host: 'tasks.ovenwand.wtf'
 	}
 });
