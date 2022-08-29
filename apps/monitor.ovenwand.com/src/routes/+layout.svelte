7189a3e34ac8b9c79c54e6cfdb3449da8396080b
Sibbe Heijne <sibbe@ovenwand.com>
2022-08-29 20:42:34 +0200

    fix: rename $app/env to $app/environment in svelte apps

--- apps/monitor.ovenwand.com/src/routes/+layout.svelte
+++ apps/monitor.ovenwand.com/src/routes/+layout.svelte
@@ -1,6 +1,6 @@
 <script lang="ts">
 	import { Base } from '@ovenwand/app';
-	import { browser } from '$app/env';
+	import { browser } from '$app/environment';
 	import { page } from '$app/stores';
 </script>
 
