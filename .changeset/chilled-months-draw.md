c3f21c61df217eefd0b2267a9dce5a81c09202e0
Sibbe Heijne <sibbe@ovenwand.com>
2022-10-12 12:50:05 +0200

    feat(ui.overlay): add ability to link overlay to another page

---
+++ .changeset/chilled-months-draw.md
@@ -0,0 +1,5 @@
+---
+'@ovenwand/ui.overlay': patch
+---
+
+Add ability to link overlay to another back using the `back` prop
--- packages/ui/overlay/src/Overlay.svelte
+++ packages/ui/overlay/src/Overlay.svelte
@@ -4,12 +4,17 @@
 
 	let className: string | null = null;
 	export { className as class };
+	export let back = null;
 
 	$: overlayClassName = createClassName({
 		[className as string]: className
 	});
 </script>
 
-<div class="fixed bg-gray-900/80 inset-0 {overlayClassName}" transition:fade on:click|self>
+<div class="fixed bg-gray-900/80 inset-0 {overlayClassName}" transition:fade>
+	<a class="fixed inset-0" href={back} title="Go back" on:click>
+		<span class="hidden">Go back</span>
+	</a>
+
 	<slot />
 </div>
