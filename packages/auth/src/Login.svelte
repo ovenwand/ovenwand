824925ba66593315c08847ffd66426e764045c91
Sibbe Heijne <sibbe@ovenwand.com>
2022-10-13 18:07:09 +0200

    feat(auth): add Login and Logout components
    - Move server side api to ./node export

---
+++ packages/auth/src/Login.svelte
@@ -0,0 +1,14 @@
+<script lang="ts">
+  import { Button } from '@ovenwand/ui.button';
+  export let action = null;
+  export let enhance;
+</script>
+
+<form method="POST" {action} use:enhance>
+  <slot name="errors"/>
+
+  <input type="email" name="email" />
+  <input type="password" name="password" />
+
+  <Button type="submit">Login</Button>
+</form>
---
+++ packages/auth/src/Logout.svelte
@@ -0,0 +1,17 @@
+<script lang="ts">
+  import { onMount } from 'svelte';
+
+  export let enhance;
+
+  let form;
+
+  onMount(() => {
+    form.submit();
+  });
+</script>
+
+<form bind:this={form} method="POST" use:enhance>
+  <noscript>
+    <button type="submit">Click here to logout</button>
+  </noscript>
+</form>
