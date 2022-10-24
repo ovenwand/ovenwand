83dd7c3102a9470524d8063a6439df35c00e84c4
Sibbe Heijne <sibbe@ovenwand.com>
2022-10-24 14:54:55 +0200

    fix(tasks.ovenwand.com): add error logging to system migrations endpoint

---
+++ .changeset/warm-turkeys-wash.md
@@ -0,0 +1,5 @@
+---
+'@ovenwand/tasks.ovenwand.com': patch
+---
+
+add error logging to system migrations endpoint
--- apps/tasks.ovenwand.com/src/routes/(app)/api/system/database/migrate/+server.ts
+++ apps/tasks.ovenwand.com/src/routes/(app)/api/system/database/migrate/+server.ts
@@ -2,9 +2,12 @@ import { json } from '@sveltejs/kit';
 import { migrate } from '$lib/database';
 
 export async function GET({ params, locals }: import('./$types').RequestEvent) {
-	const result = await migrate(params.mode || 'merge', locals.token);
-
-	console.log(result);
+	try {
+		const result = await migrate(params.mode || 'merge', locals.token);
+		console.log(result);
+	} catch (e) {
+		console.error(e);
+	}
 
 	return json({ data: 'ok' });
 }
