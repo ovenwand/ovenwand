0a43abf56cbf6321c8be8d03350b082befa13f14
Sibbe Heijne <sibbe@ovenwand.com>
2022-11-19 14:29:18 +0100

    feat(toolchain): add toolchain cli wrapper and utils

---
+++ toolchain/packages/cli/src/argv.ts
@@ -0,0 +1,1 @@
+export { argv } from 'node:process';
---
+++ toolchain/packages/cli/src/index.ts
@@ -0,0 +1,5 @@
+export * from 'commander';
+export * from './colors.js';
+export * from './exec.js';
+export * from './exit.js';
+export * from './argv.js';
