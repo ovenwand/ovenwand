0fcd462b66425803c6806ef256d34b1ecaa77e2c
Sibbe Heijne <sibbe@ovenwand.com>
2022-11-19 14:40:41 +0100

    feat(toolchain.core): add env plugin

---
+++ toolchain/packages/core/src/plugins/plugin-env/index.ts
@@ -0,0 +1,26 @@
+import { env } from 'node:process';
+import { definePlugin, PRIORITY } from '@ovenwand/toolchain.core';
+
+export const name = 'core:env';
+
+export const priority = PRIORITY.CORE;
+
+export default definePlugin((context: Toolchain.Context) => {
+	context.env = env;
+
+	context.hooks.create('environment', {
+		priority: -100,
+
+		handle: async (hook) => {
+			context.env = await hook(context.env, context);
+			return context.env;
+		},
+	});
+
+	return {
+		async configure(config) {
+			config.env = {};
+			return config;
+		},
+	};
+});
