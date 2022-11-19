1b121fd2d30b2790bc9af5e087e9159dcd79c9ef
Sibbe Heijne <sibbe@ovenwand.com>
2022-11-19 14:08:28 +0100

    feat(config): remove the need for a tailwind.config.cjs in packages

---
+++ .changeset/clever-balloons-occur.md
@@ -0,0 +1,5 @@
+---
+"@ovenwand/config": patch
+---
+
+Remove the need of creating a tailwind.config.cjs in packages by importing it into the postcss config
--- packages/config/postcss.config.cjs
+++ packages/config/postcss.config.cjs
@@ -1,6 +1,10 @@
+const tailwindcss = require('tailwindcss');
+const autoprefixer = require('autoprefixer');
+const tailwindConfig = require('./tailwind.config.cjs');
+
 module.exports = {
-	plugins: {
-		tailwindcss: {},
-		autoprefixer: {}
-	}
+	plugins: [
+		tailwindcss(tailwindConfig),
+		autoprefixer(),
+	],
 };
