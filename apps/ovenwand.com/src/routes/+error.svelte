9efa32099219014a946f5297102e730dce39edb9
Sibbe Heijne <sibbe@ovenwand.com>
2022-11-20 23:31:33 +0100

    fix(ovenwand.com): add base layout to error pages

--- apps/ovenwand.com/src/routes/+error.svelte
+++ apps/ovenwand.com/src/routes/+error.svelte
@@ -1,11 +1,30 @@
 <script lang="ts">
+	import '../app.css';
+	import { Footer, Header, Layout } from '@ovenwand/app';
 	import { Column, Grid } from '@ovenwand/ui';
 	import { page } from '$app/stores';
+	import { ApplicationSidebar, Navigation } from '$lib/components';
 </script>
 
-<Grid>
-	<Column columns={{ md: 8 }} offset={{ md: 2 }}>
-		<h1>{$page.error.status}</h1>
-		<h3>{$page.error.message}</h3>
-	</Column>
-</Grid>
+<Layout header footer>
+	<svelte:fragment slot="before-content">
+		<Header>
+			<Navigation/>
+		</Header>
+	</svelte:fragment>
+
+	<Grid class="min-h-full">
+		<Column class="flex flex-col items-center justify-center min-h-full" columns={{ md: 8 }} offset={{ md: 2 }}>
+			<h1>{$page.status}</h1>
+			<h3>{$page.error.message}</h3>
+		</Column>
+	</Grid>
+
+	<svelte:fragment slot="after-content">
+		<Footer />
+	</svelte:fragment>
+
+	<svelte:fragment slot="outside">
+<!--		<ApplicationSidebar />-->
+	</svelte:fragment>
+</Layout>
