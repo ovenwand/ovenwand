1584b6c0754c1c553a2a66945d5e68c473122de7
Sibbe Heijne <sibbe@ovenwand.com>
2022-11-20 23:27:12 +0100

    feat: add docs app

---
+++ apps/docs.ovenwand.com/.storybook/main.ts
@@ -0,0 +1,14 @@
+export default {
+  // Storybook 7 (storyStoreV7) doesn't support svelte csf yet.. See: https://github.com/storybookjs/storybook/issues/16673
+  // stories: ['../src/**/*.stories.{mdx,js,ts,jsx,tsx,svelte}'],
+
+  framework: {
+    "name": "@storybook/svelte-vite",
+    "options": {}
+  },
+
+  addons: [
+    '@ovenwand/toolchain.plugins.storybook/preset-svelte',
+    '@ovenwand/toolchain.plugins.storybook/preset-composition',
+  ],
+}
