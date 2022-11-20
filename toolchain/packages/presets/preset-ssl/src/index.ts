34635565794e80bfc5de8844cf7df12ecd02efed
Sibbe Heijne <sibbe@ovenwand.com>
2022-11-20 23:04:48 +0100

    feat(toolchain): add ssl preset

---
+++ toolchain/packages/presets/preset-ssl/src/index.ts
@@ -0,0 +1,11 @@
+import { definePreset } from '@ovenwand/toolchain.core';
+
+export const name = 'ssl';
+
+export default definePreset({
+	async configure(config) {
+		const { default: viteConfig } = await import('./vite.config.js');
+		config.vite.configs.push(viteConfig);
+		return config;
+	},
+});
