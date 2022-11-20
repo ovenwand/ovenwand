30731a63278ef86e90e81fec4995d92e9f0c18f0
Sibbe Heijne <sibbe@ovenwand.com>
2022-11-20 22:55:46 +0100

    feat(toolchain): add config plugin

---
+++ toolchain/packages/plugins/plugin-config/src/index.ts
@@ -0,0 +1,20 @@
+import { definePlugin, PRIORITY } from '@ovenwand/toolchain.core';
+
+export const name = 'config';
+
+export const priority = PRIORITY.CORE;
+
+export default definePlugin((context) => {
+	context.config = {};
+
+	context.hooks.create('configure', {
+		priority: -50,
+
+		handle: async (hook) => {
+			context.config = await hook(context.config, context);
+			return context.config;
+		}
+	});
+
+	return {};
+});
