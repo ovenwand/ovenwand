d5bc6d7f34e800d3d56220ac7aab4c399ff483cb
Sibbe Heijne <sibbe@ovenwand.com>
2022-11-20 23:18:36 +0100

    feat(monitor.ovenwand.com): replace kit with toolchain

--- apps/monitor.ovenwand.com/vite.config.ts
+++ apps/monitor.ovenwand.com/vite.config.ts
@@ -2,26 +2,12 @@ import { defineConfig } from 'vite';
 import { sveltekit } from '@sveltejs/kit/vite';
 
 export default defineConfig({
-	legacy: { buildSsrCjsExternalHeuristics: true }, // TODO Hopefully remove one day
-
-	plugins: [sveltekit()],
+	plugins: [
+		sveltekit(), // TODO Figure out how to move this to @ovenwand/toolchain.presets.sveltekit
+	],
 
 	envPrefix: [
-		'VITE_',
-		'PUBLIC_',
 		'VERCEL_',
 		'SENTRY_' // TODO remove SENTRY_ from prefixes
 	],
-
-	define: {
-		'import.meta.env.APP_VERSION': JSON.stringify(process.env.npm_package_version)
-	},
-
-	server: {
-		host: 'monitor.ovenwand.wtf'
-	},
-
-	ssr: {
-		noExternal: ['@ovenwand/**'] // TODO hopefully remove one day?
-	}
 });
