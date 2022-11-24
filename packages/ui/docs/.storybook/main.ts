bd00899d285e14f4d36bae373a87c5f19d51c756
Sibbe Heijne <sibbe@ovenwand.com>
2022-11-24 11:18:34 +0100

    feat(ui.docs): add base and bundle storybook presets

--- packages/ui/docs/.storybook/main.ts
+++ packages/ui/docs/.storybook/main.ts
@@ -5,6 +5,8 @@ export default {
   },
 
   addons: [
+    '@ovenwand/toolchain.plugins.storybook/preset',
+    '@ovenwand/toolchain.plugins.storybook/preset-bundle',
     '@ovenwand/toolchain.plugins.storybook/preset-svelte',
   ],
 }
