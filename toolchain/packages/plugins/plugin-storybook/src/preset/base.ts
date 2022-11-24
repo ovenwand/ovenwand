d94e5395b121e9dcd73da2740e51975a4980a816
Sibbe Heijne <sibbe@ovenwand.com>
2022-11-24 04:07:13 +0100

    fix(toolchain.plugins.storybook): dynamically import globby to prevent esbuild bug

--- toolchain/packages/plugins/plugin-storybook/src/preset/base.ts
+++ toolchain/packages/plugins/plugin-storybook/src/preset/base.ts
@@ -1,22 +1,23 @@
 import { resolve } from 'node:path';
-import { globbySync } from 'globby';
 
-function findStories({ include = [], exclude = [] } = {}) {
-	return globbySync([...include, ...exclude.map((pattern) => `!${pattern}`)], {
+async function findStories({ include = [], exclude = [] } = {}) {
+	const { globby } = await import('globby');
+
+	return await globby([...include, ...exclude.map((pattern) => `!${pattern}`)], {
 		cwd: resolve('.storybook')
 	});
 }
 
 export default {
-	stories: findStories({
+	stories: async (stories = []) => [...stories, ...await findStories({
 		// Storybook 7 (storyStoreV7) doesn't support svelte csf yet.. See: https://github.com/storybookjs/storybook/issues/16673
 		// include: ["../**/src/**/*.stories.@(mdx|js|ts|jsx|tsx|svelte)"],
 		include: [
-			"../../**/packages/**/*.stories.@(mdx|js|ts|jsx|tsx)",
+			// "../../**/packages/**/*.stories.@(mdx|js|ts|jsx|tsx)",
 			"../**/src/**/*.stories.@(mdx|js|ts|jsx|tsx)"
 		],
 		exclude: ["../**/node_modules"]
-	}),
+	})],
 
 	addons: [
 		'@storybook/addon-links',
