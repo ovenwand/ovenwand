b83fe22a7bf1066147eee26e624e91c305f677b1
Sibbe Heijne <sibbe@ovenwand.com>
2022-11-24 11:16:41 +0100

    feat(toolchain.plugins.storybook): add bundle preset

--- toolchain/packages/plugins/plugin-storybook/src/preset/bundle.ts
+++ toolchain/packages/plugins/plugin-storybook/src/preset/bundle.ts
@@ -4,15 +4,10 @@ export default {
 	stories: async (stories = []) => [...stories, ...await findStories({
 		// Storybook 7 (storyStoreV7) doesn't support svelte csf yet.. See: https://github.com/storybookjs/storybook/issues/16673
 		// include: ["../**/src/**/*.stories.@(mdx|js|ts|jsx|tsx|svelte)"],
-		include: [
-			"../src/**/*.stories.@(mdx|js|ts|jsx|tsx)"
-		],
-		exclude: ["../**/node_modules"]
+		include: ["../../**/packages/**/*.stories.@(mdx|js|ts|jsx|tsx)"],
+		exclude: ["../../**/node_modules"]
 	})],
 
 	addons: [
-		'@storybook/addon-links',
-		'@storybook/addon-essentials',
-		'@storybook/addon-interactions',
 	],
 };
