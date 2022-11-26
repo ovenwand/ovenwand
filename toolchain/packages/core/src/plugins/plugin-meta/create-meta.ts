922f15913c858094cfe24b1b9aaa4b75e413982e
Sibbe Heijne <sibbe@ovenwand.com>
2022-11-26 18:59:16 +0100

    refactor(toolchain.core): clean up plugin meta

--- toolchain/packages/core/src/plugins/plugin-meta/create-meta.ts
+++ toolchain/packages/core/src/plugins/plugin-meta/create-meta.ts
@@ -1,17 +1,17 @@
-import { cwd } from 'node:process';
+import { cwd as _cwd } from 'node:process';
 import { dirname, resolve } from 'node:path';
 import { findPackageJson, readPackageJson, findWorkspace, detectPackageManager } from '../../utils/index.js';
 
 export async function createMeta(context: Toolchain.Context): Promise<Toolchain.Meta> {
-	const workspacePath = await findWorkspace();
+	const cwd = _cwd();
+	const workspacePath = await findWorkspace(cwd);
 	const workspacePackagePath = resolve(workspacePath, 'package.json');
+	const localPackagePath = await findPackageJson(cwd, workspacePath);
 
-	context.cwd ??= cwd();
-
-	const localPackagePath = await findPackageJson(context.cwd, workspacePath);
+	context.cwd ??= cwd; // TODO remove
 
 	return {
-		cwd: context.cwd,
+		cwd,
 
 		packageManager: await detectPackageManager(),
 
