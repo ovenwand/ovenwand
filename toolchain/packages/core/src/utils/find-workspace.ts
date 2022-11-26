02f2237e7858688c2127c5250254022f65b5c8b5
Sibbe Heijne <sibbe@ovenwand.com>
2022-11-26 19:20:43 +0100

    feat(toolchain.core): allow passing cwd to "findWorkspace" util

--- toolchain/packages/core/src/utils/find-workspace.ts
+++ toolchain/packages/core/src/utils/find-workspace.ts
@@ -1,6 +1,6 @@
-import { cwd } from 'node:process';
+import { cwd as _cwd } from 'node:process';
 import findWorkspaceDir from '@pnpm/find-workspace-dir';
 
-export async function findWorkspace() {
-	return findWorkspaceDir.default(cwd());
+export async function findWorkspace(cwd = _cwd()) {
+	return findWorkspaceDir.default(cwd);
 }
