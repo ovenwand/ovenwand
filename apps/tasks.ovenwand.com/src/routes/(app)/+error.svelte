9577c6683328df9b4e8fe413938a039028173af9
Sibbe Heijne <sibbe@ovenwand.com>
2022-11-27 01:29:56 +0100

    feat(tasks.ovenwand.com): cleanup error page

---
+++ apps/tasks.ovenwand.com/src/routes/(app)/+error.svelte
@@ -0,0 +1,11 @@
+<script lang="ts">
+	import { Column, Grid } from '@ovenwand/ui';
+	import { page } from '$app/stores';
+</script>
+
+<Grid class="min-h-full" relative>
+	<Column class="flex flex-col items-center justify-center">
+		<h2 class="text-5xl font-extrabold">{$page.status}</h2>
+		<h1 class="text-4xl font-bold mt-2">{$page.error.message}</h1>
+	</Column>
+</Grid>
