5af3598e13f2264353242a15fd423d10a18112c6
Sibbe Heijne <sibbe@ovenwand.com>
2022-11-27 02:32:45 +0100

    feat(tasks.ovenwand.com): improve login page ux

--- apps/tasks.ovenwand.com/src/routes/(app)/account/login/+page.svelte
+++ apps/tasks.ovenwand.com/src/routes/(app)/account/login/+page.svelte
@@ -1,17 +1,20 @@
 <script lang="ts">
 	import { Column, Grid } from '@ovenwand/ui';
 	import { Login } from '@ovenwand/auth';
+	import { Panel } from '$lib/components';
 	export let form;
 </script>
 
-<Grid>
-	<Column>
-		<Login>
-			<svelte:fragment slot="errors">
-				{#if form?.form}
-					<p>{form.form}</p>
-				{/if}
-			</svelte:fragment>
-		</Login>
+<Grid class="min-h-full" relative>
+	<Column class="flex flex-col items-center justify-center">
+		<Panel title="Login">
+			<Login>
+				<svelte:fragment slot="errors">
+					{#if form?.form}
+						<p>{form.form}</p>
+					{/if}
+				</svelte:fragment>
+			</Login>
+		</Panel>
 	</Column>
 </Grid>
