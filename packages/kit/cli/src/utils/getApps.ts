d9b6cd98c310fda7c0afbee7e07fa542fdbe6476
Sibbe Heijne <sibbe@ovenwand.com>
2022-11-28 11:21:37 +0100

    chore(kit.cli): update @pnpm/find-workspace-dir to v5

--- packages/kit/cli/src/utils/getApps.ts
+++ packages/kit/cli/src/utils/getApps.ts
@@ -1,10 +1,10 @@
 import { resolve } from 'path';
 import { promises as fsp } from 'fs';
-import findWorkspaceDir from '@pnpm/find-workspace-dir';
+import { findWorkspaceDir } from '@pnpm/find-workspace-dir';
 
 export async function getApps() {
 	const apps = [];
-	const workspace = await findWorkspaceDir.default(process.cwd()); // TODO support any kind of workspace: npm, yarn, etc
+	const workspace = await findWorkspaceDir(process.cwd()); // TODO support any kind of workspace: npm, yarn, etc
 	const appsDir = resolve(workspace, 'apps'); // TODO
 	const dirs = await fsp.readdir(appsDir);
 
