7f227031e1aa71b8cfe726a80a3606c02c63869f
Sibbe Heijne <sibbe@ovenwand.com>
2022-11-29 15:39:50 +0100

    feat(toolchain.plugins.doppler): improve performance and debugging

--- toolchain/packages/plugins/plugin-doppler/src/create-config.ts
+++ toolchain/packages/plugins/plugin-doppler/src/create-config.ts
@@ -1,14 +1,12 @@
 export function createConfig({ env, meta }) {
-	const { name: packageName } = meta.package
-		? meta.package.manifest
-		: meta.workspace.manifest;
+	const { name: packageName } = meta.package ? meta.package.manifest : meta.workspace.manifest;
 
 	return {
-		enabled: !['false', '0'].includes(env.OVEN_DOPPLER_ENV),
-		configFile: env.OVEN_DOPPLER_CONFIG,
+		enabled: !['false', '0'].includes(env.TOOLCHAIN_DOPPLER),
+		configFile: env.TOOLCHAIN_DOPPLER_CONFIG,
 		scope: {
 			project: packageName.split('/')[1].replace(/\./g, '-'),
-			config: env.NODE_ENV ?? 'development',
-		},
+			config: env.NODE_ENV ?? 'development'
+		}
 	};
 }
--- toolchain/packages/plugins/plugin-doppler/src/index.ts
+++ toolchain/packages/plugins/plugin-doppler/src/index.ts
@@ -11,7 +11,7 @@ async function doesDopplerConfigExist(context) {
 	try {
 		await access(doppler.configFile);
 		return true;
-	} catch(e) {
+	} catch (e) {
 		return false;
 	}
 }
@@ -19,22 +19,61 @@ async function doesDopplerConfigExist(context) {
 export const name = 'doppler';
 
 export default definePlugin((context) => {
+	const debug = context.logger.debug('toolchain:plugin-doppler');
+
 	return {
-		async environment(env) {
-			env.OVEN_DOPPLER_ENV = env.OVEN_DOPPLER_ENV ?? String(Number(!!await findExecutable('doppler')));
-			env.OVEN_DOPPLER_CONFIG = env.OVEN_DOPPLER_CONFIG ?? resolve(context.meta.workspace.path, '.kit/config/.doppler.yaml'); // TODO find proper location to store doppler config
+		async environment(env, { meta }) {
+			debug('Looking for doppler executable in $PATH...');
+
+			const dopplerBinPath = await findExecutable('doppler'); // TODO improve UX when doppler bin is not found
+
+			debug(
+				dopplerBinPath
+					? `Found doppler bin at "${dopplerBinPath}"`
+					: 'Could not find doppler bin, disabling doppler environment'
+			);
+
+			if (!dopplerBinPath || env.TOOLCHAIN_DOPPLER == null) {
+				env.TOOLCHAIN_DOPPLER = String(Number(Boolean(dopplerBinPath)));
+			}
+
+			env.TOOLCHAIN_DOPPLER_CONFIG ??= resolve(meta.workspace.path, '.kit/config/.doppler.yaml'); // TODO find proper location to store doppler config
 
 			const doppler = createConfig(context);
 
 			// Set config early and freeze to prevent differences in the environment and config
 			// before and after resolution, so the environment can be used reliably for configuration.
+			// TODO decide if this is the right thing to do
 			context.config.doppler = Object.freeze(doppler);
 
-			if (!doppler.enabled || !await doesDopplerConfigExist(context)) {
+			if (!doppler.enabled) {
+				debug('Will not load doppler environment, doppler has been disabled');
 				return env;
 			}
 
-			return await loadDopplerEnv(context);
+			debug('Looking for doppler config...');
+
+			if (!(await doesDopplerConfigExist(context))) {
+				// TODO improve UX when doppler config is not found
+				debug(`Could not find doppler config at: ${env.TOOLCHAIN_DOPPLER_CONFIG}`);
+				return env;
+			}
+
+			debug(`Found doppler config at "${env.TOOLCHAIN_DOPPLER_CONFIG}"`);
+
+			const { env: dopplerEnv, workspaceEnv, localEnv } = await loadDopplerEnv(context, debug);
+
+			if (workspaceEnv) {
+				debug(`Loaded environment for "${meta.workspace.manifest.name}":`);
+				debug(JSON.stringify(workspaceEnv, null, '  '));
+			}
+
+			if (localEnv) {
+				debug(`Loaded environment for "${meta.package.manifest.name}":`);
+				debug(JSON.stringify(localEnv, null, '  '));
+			}
+
+			return dopplerEnv;
 		},
 
 		async resolve({ cli, config }) {
@@ -42,9 +81,7 @@ export default definePlugin((context) => {
 				return;
 			}
 
-			cli.commands.add(
-				createEnvCommand(context)
-			);
+			cli.commands.add(createEnvCommand(context));
 		}
 	};
 });
