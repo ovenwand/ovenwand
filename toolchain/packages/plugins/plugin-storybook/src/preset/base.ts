c80fc0cc846684f5ab2cedaf27b0763812be0312
Sibbe Heijne <sibbe@ovenwand.com>
2023-04-15 16:05:19 +0200

    chore(docs): update storybook

--- toolchain/packages/plugins/plugin-storybook/src/preset/base.ts
+++ toolchain/packages/plugins/plugin-storybook/src/preset/base.ts
@@ -1,18 +1,15 @@
 import { findStories } from '../utils';
 
 export default {
-	stories: async (stories = []) => [...stories, ...await findStories({
-		// Storybook 7 (storyStoreV7) doesn't support svelte csf yet.. See: https://github.com/storybookjs/storybook/issues/16673
-		// include: ["../**/src/**/*.stories.@(mdx|js|ts|jsx|tsx|svelte)"],
-		include: [
-			"../src/**/*.stories.@(mdx|js|ts|jsx|tsx)"
-		],
-		exclude: ["../**/node_modules"]
-	})],
-
-	addons: [
-		'@storybook/addon-links',
-		'@storybook/addon-essentials',
-		'@storybook/addon-interactions',
+	stories: async (stories = []) => [
+		...stories,
+		...(await findStories({
+			// Storybook 7 (storyStoreV7) doesn't support svelte csf yet.. See: https://github.com/storybookjs/storybook/issues/16673
+			// include: ["../**/src/**/*.stories.@(mdx|js|ts|jsx|tsx|svelte)"],
+			include: ['../src/**/*.stories.@(mdx|js|ts|jsx|tsx)'],
+			exclude: ['../**/node_modules']
+		}))
 	],
+
+	addons: ['@storybook/addon-essentials']
 };
