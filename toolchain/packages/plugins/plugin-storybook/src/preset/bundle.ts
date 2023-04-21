24dba6a0e4b4d3486fa4ba1db47dcf28b999ddd8
Sibbe Heijne <sibbe@ovenwand.com>
2023-04-21 22:13:25 +0200

    feat(toolchain.plugins.storybook): re-enable addon-svelte-csf

--- toolchain/packages/plugins/plugin-storybook/src/preset/bundle.ts
+++ toolchain/packages/plugins/plugin-storybook/src/preset/bundle.ts
@@ -4,9 +4,7 @@ export default {
 	stories: async (stories = []) => [
 		...stories,
 		...(await findStories({
-			// Storybook 7 (storyStoreV7) doesn't support svelte csf yet.. See: https://github.com/storybookjs/storybook/issues/16673
-			// include: ["../**/src/**/*.stories.@(mdx|js|ts|jsx|tsx|svelte)"],
-			include: ['../../**/packages/**/*.stories.@(mdx|js|ts|jsx|tsx)'],
+			include: ['../../**/packages/**/*.stories.@(mdx|js|ts|jsx|tsx|svelte)'],
 			exclude: ['../../**/node_modules']
 		}))
 	],
