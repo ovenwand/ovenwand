6e87986785fa4c61cfea9a11796cabcf65f6d547
Sibbe Heijne <sibbe@ovenwand.com>
2023-04-21 22:16:44 +0200

    refactor(ui.docs): move button stories to svelte csf

---
+++ packages/ui/packages/atoms/button/src/Button.stories.svelte
@@ -0,0 +1,55 @@
+<script>
+	import { Meta, Story, Template } from '@storybook/addon-svelte-csf';
+	import Button from './Button.svelte';
+</script>
+
+<Meta
+	title="Atoms/Button"
+	component={Button}
+	argTypes={{
+		size: {
+			control: 'radio',
+			options: ['default', 'large']
+		}
+	}}
+/>
+
+<Template let:args>
+	<Button {...args}>
+		{args.default ?? 'Button'}
+	</Button>
+</Template>
+
+<Story name="Default" />
+
+<Story
+	name="Large"
+	args={{
+		size: 'large',
+		default: 'Large button'
+	}}
+/>
+
+<Story
+	name="Full width"
+	args={{
+		full: true,
+		default: 'Full width button'
+	}}
+/>
+
+<Story
+	name="Anchor"
+	args={{
+		href: '#',
+		default: 'Anchor button'
+	}}
+/>
+
+<Story
+	name="Submit button"
+	args={{
+		type: 'submit',
+		default: 'Submit button'
+	}}
+/>
--- packages/ui/packages/atoms/button/src/stories/Default.template.svelte
+++
@@ -1,9 +0,0 @@
-<script lang="ts">
-	import Button from '../Button.svelte';
-	export let props;
-	export let slots;
-</script>
-
-<Button {...props}>
-	{slots.default}
-</Button>
