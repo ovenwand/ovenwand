b79bc4ff7a1d7c4aec395b6a7143b80be67004dc
Sibbe Heijne <sibbe@ovenwand.com>
2023-04-22 12:11:53 +0200

    refactor(ui.docs): move notification stories to svelte csf

--- packages/ui/packages/molecules/notification/src/notification/stories/Default.template.svelte
+++
@@ -1,9 +0,0 @@
-<script lang="ts">
-	import Notification from '../Notification.svelte';
-	export let props;
-	export let slots;
-</script>
-
-<Notification {...props}>
-	{slots.default}
-</Notification>
