f329089998b9e942f79f17902f55c7e440fdda6c
Sibbe Heijne <sibbe@ovenwand.com>
2023-04-22 11:54:43 +0200

    refactor(ui.docs): move modal stories to svelte csf

---
+++ packages/ui/packages/molecules/modal/src/Modal.stories.svelte
@@ -0,0 +1,16 @@
+<script>
+	import { Meta, Story, Template } from '@storybook/addon-svelte-csf';
+	import Modal from './Modal.svelte';
+</script>
+
+<Meta title="Molecules/Modal" component={Modal} />
+
+<Template let:args>
+	<Modal {...args}>
+		{args.default ?? 'Content'}
+	</Modal>
+</Template>
+
+<Story name="Default" />
+
+<Story name="With back link" args={{ active: true, back: '#' }} />
