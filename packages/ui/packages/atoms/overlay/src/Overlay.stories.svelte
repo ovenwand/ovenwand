684a7833907394a5bfa9195a076de696f43b1bc0
Sibbe Heijne <sibbe@ovenwand.com>
2023-04-22 11:39:16 +0200

    docs(ui.overlay): add default and with back link stories

---
+++ packages/ui/packages/atoms/overlay/src/Overlay.stories.svelte
@@ -0,0 +1,27 @@
+<script>
+	import { Meta, Story, Template } from '@storybook/addon-svelte-csf';
+	import Overlay from './Overlay.svelte';
+</script>
+
+<Meta
+	title="Atoms/Overlay"
+	component={Overlay}
+	parameters={{
+		layout: 'fullscreen',
+		docs: {
+			story: {
+				inline: false
+			}
+		}
+	}}
+/>
+
+<Template let:args>
+	<Overlay {...args}>
+		{args.default ?? 'Content'}
+	</Overlay>
+</Template>
+
+<Story name="Default" />
+
+<Story name="With back link" args={{ back: '#' }} />
