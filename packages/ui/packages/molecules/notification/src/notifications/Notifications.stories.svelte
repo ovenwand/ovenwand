33020733b5460d421d7a5512f6ce9468fc00da1c
Sibbe Heijne <sibbe@ovenwand.com>
2023-04-22 12:11:53 +0200

    refactor(ui.docs): move notification stories to svelte csf

---
+++ packages/ui/packages/molecules/notification/src/notifications/Notifications.stories.svelte
@@ -0,0 +1,20 @@
+<script>
+	import { Meta, Story, Template } from '@storybook/addon-svelte-csf';
+	import { useNotifications } from '../store';
+	import Notifications from './Notifications.svelte';
+
+	const { info, error, notifications } = useNotifications();
+
+	if (!$notifications.length) {
+		info({ message: 'Info notification' });
+		error({ message: 'Error notification' });
+	}
+</script>
+
+<Meta title="Molecules/Notifications" component={Notifications} />
+
+<Template let:args>
+	<Notifications {...args} />
+</Template>
+
+<Story name="Default" />
