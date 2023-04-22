e9d5446c0cba1b96761f9bfd7a501c3a2d0c5786
Sibbe Heijne <sibbe@ovenwand.com>
2023-04-22 09:27:31 +0200

    docs(ui.icon): add default story

---
+++ packages/ui/packages/atoms/icon/src/Icon.stories.svelte
@@ -0,0 +1,14 @@
+<script>
+	import { Meta, Story, Template } from '@storybook/addon-svelte-csf';
+	import Icon from './Icon.svelte';
+</script>
+
+<Meta title="Atoms/Icon" component={Icon} />
+
+<Template let:args>
+	<Icon {...args}>
+		{@html args.icon ?? 'X'}
+	</Icon>
+</Template>
+
+<Story name="Default" />
