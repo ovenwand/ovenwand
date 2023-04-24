d74b4c13fe055e3bae39e880efcbcc27f65c0df6
Sibbe Heijne <sibbe@ovenwand.com>
2023-04-24 20:35:57 +0200

    feat(ui.docs): enable autodocs

--- packages/ui/docs/.storybook/main.ts
+++ packages/ui/docs/.storybook/main.ts
@@ -5,5 +5,9 @@ export default {
 		'@ovenwand/toolchain.plugins.storybook/preset',
 		'@ovenwand/toolchain.plugins.storybook/preset-bundle',
 		'@ovenwand/toolchain.plugins.storybook/preset-svelte'
-	]
+	],
+
+	docs: {
+		autodocs: true
+	}
 };
