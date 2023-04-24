2710bb0c712cf721a2b480b2e9952ffb2262e533
Sibbe Heijne <sibbe@ovenwand.com>
2023-04-24 20:38:54 +0200

    refactor(ui.docs): move calendar stories to svelte csf

---
+++ packages/ui/packages/organisms/calendar/src/Calendar.stories.svelte
@@ -0,0 +1,18 @@
+<script>
+	import { Meta, Story, Template } from '@storybook/addon-svelte-csf';
+	import Calendar from './Calendar.svelte';
+</script>
+
+<Meta
+	title="Organisms/Calendar"
+	component={Calendar}
+	argTypes={{
+		date: { control: 'date' } // Regex matching does not seem to be working
+	}}
+/>
+
+<Template let:args>
+	<Calendar {...args} />
+</Template>
+
+<Story name="Default" />
--- packages/ui/packages/organisms/calendar/src/stories/Default.template.svelte
+++
@@ -1,6 +0,0 @@
-<script lang="ts">
-	import Calendar from '../Calendar.svelte';
-	export let props;
-</script>
-
-<Calendar {...props} />
