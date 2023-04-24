728eca3166fe39657a50de350125c042eca60fdb
Sibbe Heijne <sibbe@ovenwand.com>
2023-04-24 20:38:19 +0200

    refactor(ui.docs): move grid stories to svelte csf

---
+++ packages/ui/packages/atoms/grid/src/column/Column.stories.svelte
@@ -0,0 +1,34 @@
+<script>
+	import { Meta, Story, Template } from '@storybook/addon-svelte-csf';
+	import Grid from '../grid/Grid.svelte';
+	import Column from './Column.svelte';
+</script>
+
+<Meta
+	title="Atoms/Grid/Column"
+	components={Column}
+	parameters={{
+		layout: 'fullscreen',
+		docs: {
+			story: {
+				inline: false
+			}
+		}
+	}}
+/>
+
+<Template let:args>
+	<Grid>
+		<Column {...args}>
+			{JSON.stringify(args.columns ?? 12)} ({JSON.stringify(args.offset ?? 0)})
+		</Column>
+	</Grid>
+</Template>
+
+<Story name="Default" />
+
+<Story name="Columns" args={{ columns: 6 }} />
+
+<Story name="Offset" args={{ columns: 6, offset: 6 }} />
+
+<Story name="Breakpoints" args={{ columns: { md: 6 }, offset: { md: 6 } }} />
